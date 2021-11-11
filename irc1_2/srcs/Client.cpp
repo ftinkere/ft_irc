2eddc12796abb88c88c1c4037a5433c57f35b1ac
latkins <46654345@mail.ru>
2021-11-11 19:42:22 +0300

    parse

---
+++ irc1_2/srcs/Client.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Frey Tinkerer on 11/11/21.
+//
+
+#include "Client.hpp"
+
+IRC::is_nickname_s IRC::is_nickname(std::string nickname) {
+		return is_nickname_s(nickname);
+}
+
+IRC::is_fd_s IRC::is_fd(int fd) {
+	return is_fd_s(fd);
+
+}

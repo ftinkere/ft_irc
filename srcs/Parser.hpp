4edc92b27907260b3a3cb48c9526ca6666b051c3
ftinkere <ftinkere@ya.ru>
2021-11-11 20:05:54 +0300

    parser

--- srcs/Parser.hpp
+++ srcs/Parser.hpp
@@ -5,6 +5,11 @@
 #ifndef FT_IRC_PARSER_HPP
 #define FT_IRC_PARSER_HPP
 
+#include <vector>
+#include "Client.hpp"
+
+namespace IRC {
+
 #define SYM_COLON ':'
 
 #define MAX_LEN_NICK 9
@@ -45,10 +50,23 @@
 #define CMD_RESTART "RESTART"
 #define CMD_WALLOPS "WALLOPS"
 
+#define ERROR_ 1
+#define SUCSSES_ 0
+#define TWO_WORDS -1
 
-class Parser {
-
-};
+	class Parser {
+	public:
+		typedef std::vector<Client>& Base;
+		Parser(Base clients, std::string buf, int fd);
+		~Parser();
+	private:
+		std::vector<std::string> params;
+		Client* id;
+		int prefix_parsing(std::string &buf);// парсим первое слово
+		int _parsing_command_word(std::string &buf, int &first_char);// парсим второе слово
+		int _parsing_param_word(std::string &buf, int &first_char);// парсим третье слово
+	};
+}
 
 
 #endif //FT_IRC_PARSER_HPP

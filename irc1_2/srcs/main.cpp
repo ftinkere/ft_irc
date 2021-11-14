fc31e715da037894009b19e2832afb254e9528b6
latkins <46654345@mail.ru>
2021-11-14 19:11:40 +0300

    ffrgf

---
+++ irc1_2/srcs/main.cpp
@@ -0,0 +1,10 @@
+#include "ListeningSocket.hpp"
+
+#define PORT "9034"   // порт, который мы слушаем
+
+
+int main(void)
+{
+    IRC::Socket *sock = new IRC::ListenSocket(PORT);
+    delete sock;
+}
